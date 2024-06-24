04ab3d1b6e19ba58b80591830f69e38a73a4a3df
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-24 19:30:09 +1000

    Initial commit: adding go.mod, go.sum, and main.go files

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
+)
+
+func main() {
+	llm, _ := ollama.New(ollama.WithModel("llama3"))
+
+	cmd := exec.Command("git", "status")
+
+	output, _ := cmd.CombinedOutput()
+
+	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, "It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT here is the results of git status: "+string(output))
+
+	cmd = exec.Command("git", "add", ".")
+	cmd.Run()
+	cmd = exec.Command("git", "commit", "-m", res)
+	cmd.Run()
+
+	fmt.Println("Commited", res)
+}
