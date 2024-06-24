4162b2937de571db70f8423f4a8a773c938b7034
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-06-24 20:48:00 +1000

    Update main.go file with modifications not yet staged for commit.

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 	output, _ := cmd.CombinedOutput()
 
-	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, "It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT here is the results of git status: "+string(output))
+	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, "It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make very detailed message here is the results of git status: "+string(output))
 
 	cmd = exec.Command("git", "add", ".")
 	cmd.Run()
