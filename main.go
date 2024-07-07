5e3ea7ef9be57cc21c6c5f5560d058af27581d8e
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-07 13:03:56 +1000

    Revert "Update main.go file"
    This reverts commit 7f16bec1460fbb7273ec3caf8fc66e115d959f57.

--- main.go
+++ main.go
@@ -16,11 +16,11 @@ func main() {
 
 	output, _ := cmd.CombinedOutput()
 
-	onelinelogcmd := exec.Command("git", "log", "--oneline")
+	diffcmd := exec.Command("git", "log", "--oneline")
 
-	onelinelogoutput, _ := onelinelogcmd.CombinedOutput()
+	diffoutput, _ := diffcmd.CombinedOutput()
 
-	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, fmt.Sprintf("It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make it very short make sure to use the context that you have, the results of git status: %s and here is the output of git log --oneline: %s rember to make your commit message short I perfer under 50 chars, YOU CANNOT INCLUDE THE GIT DIFF IN THE COMMIT MESSAGE YOU MUST ONLY RETURN THE COMMIT MESSAGE AND NOTHING ELSE", string(output), string(onelinelogoutput)))
+	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, fmt.Sprintf("It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make it very short make sure to use the context that you have, the results of git status: %s and here is the output of git log --oneline: %s rember to make your commit message short I perfer under 50 chars, YOU CANNOT INCLUDE THE GIT DIFF IN THE COMMIT MESSAGE YOU MUST ONLY RETURN THE COMMIT MESSAGE AND NOTHING ELSE", string(output), string(diffoutput)))
 
 	cmd = exec.Command("git", "add", ".")
 	cmd.Run()
