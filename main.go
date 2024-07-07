09963540f2c0c93e9e5a4c865e7a78c454505bd3
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-07-07 13:04:06 +1000

    Revert "Update main.go file"
    This reverts commit cee02220506cc7e063a321d54b984b46a9082259.

--- main.go
+++ main.go
@@ -16,11 +16,7 @@ func main() {
 
 	output, _ := cmd.CombinedOutput()
 
-	diffcmd := exec.Command("git", "log", "--oneline")
-
-	diffoutput, _ := diffcmd.CombinedOutput()
-
-	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, fmt.Sprintf("It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make it very short make sure to use the context that you have, the results of git status: %s and here is the output of git log --oneline: %s rember to make your commit message short I perfer under 50 chars, YOU CANNOT INCLUDE THE GIT DIFF IN THE COMMIT MESSAGE YOU MUST ONLY RETURN THE COMMIT MESSAGE AND NOTHING ELSE", string(output), string(diffoutput)))
+	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, "It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make detailed but short make sure to use the context that you have, the results of git status: "+string(output))
 
 	cmd = exec.Command("git", "add", ".")
 	cmd.Run()
