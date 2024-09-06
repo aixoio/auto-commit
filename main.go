c6d1a7b825a1975a7e79cfa5b629b861d47e3061
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-09-06 20:38:42 +1000

    feat: Update main.go

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
-  llm, _ := ollama.New(ollama.WithModel("qwen2:1.5b"))
+  llm, _ := ollama.New(ollama.WithModel("gemma2:2b"))
 
 	cmd := exec.Command("git", "status")
 
 	output, _ := cmd.CombinedOutput()
 
-	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, "It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make detailed but short make sure to use the context that you have, the results of git status do not use quotes in your answer: "+string(output))
+	res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, "It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make detailed but short make sure to use the context that you have, the results of git status: "+string(output))
 
 	cmd = exec.Command("git", "add", ".")
 	cmd.Run()
