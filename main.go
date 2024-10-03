0113341774fc08373a15fbb4f6e3796c58ec9ed9
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-10-03 10:44:28 +1000

    Added file to staging.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-  llm, _ := ollama.New(ollama.WithModel("llama3.2"))
+  llm, _ := ollama.New(ollama.WithModel("llama3.2:1b"))
 
 	cmd := exec.Command("git", "status")
 
