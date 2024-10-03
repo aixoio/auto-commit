4e905375ba77c327b16f20f9a4f9356f4900a4d0
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-10-03 10:46:56 +1000

    Deleted main.go, updated dependency versions in vendor/

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	output, _ := cmd.CombinedOutput()
 
-  res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, "It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make detailed but short make sure to use the context that you have, the results of git status you CANNOT use 'feat' to prefix your comiit, don't talk about per proeject problems only what was changed YOU CANNOT TALK ABOUT THE PROJECT IT SELF AND YOU CAN ONLY TALK ABOUT WHAT WAS CHANGED your message should look like \"Deleted main.go\": "+string(output))
+  res, _ := llms.GenerateFromSinglePrompt(context.TODO(), llm, "It is your job to write a git commit summary for this commit YOU MUST ONLY RESPOND WITH THE MESSAGE NO MORE TEXT also please make detailed but short make sure to use the context that you have, the results of git status you CANNOT use 'feat' to prefix your comiit, don't talk about per proeject problems only what was changed YOU CANNOT TALK ABOUT THE PROJECT IT SELF AND YOU CAN ONLY TALK ABOUT WHAT WAS CHANGED your message should look like \"Deleted main.go\" but change it to match the changes made to the project: "+string(output))
 
 	cmd = exec.Command("git", "add", ".")
 	cmd.Run()
